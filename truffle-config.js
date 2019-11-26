c9b9678abb7327883728dded703ed7152952470f
tak1827 <re795h@gmail.com>
2019-11-26 19:50:27 +0900

    Add rinkeby setting & Migrate contract on dev

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,12 @@
 const path = require("path");
 
+const ethwallet = require('ethereumjs-wallet');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const wallet = ethwallet.fromPrivateKey(Buffer.from(process.env.ETH_DEPLOYER_KEY_IPFS, 'hex'));
+const address = "0x" + wallet.getAddress().toString("hex");
+const provider = new HDWalletProvider(process.env.ETH_DEPLOYER_KEY_IPFS, process.env.ETH_NODE_URL);
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -10,6 +17,25 @@ module.exports = {
      port: 8545,
      network_id: 15,
      gas: 7000000,
+    },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: provider,
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+    mainnet: {
+      provider: provider,
+      network_id: 1,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
   },
   compilers: {
